ee8478edf8acee55f71c199044b11c27ae351faf
Etudiant <Etudiant>
2017-04-11 10:01:20 +0200

    Révision du 11 avril

---
+++ 10avril/js/script.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+
+    // Fermer la modal
+    $('.fa-times').click(function(){
+        $('div').fadeOut();
+    });
+
+    // Supprimer les class error
+    $('input, select, textarea').focus(function(){
+        $(this).removeClass('error');
+    });
+
+    // Capter la soumission du formulaire
+    $('form').submit(function(evt){
+
+        // Bloquer le comportement naturel du formulaire
+        evt.preventDefault();
+
+        // Définir les variables globales
+        var userName = $('[placeholder="Your name*"]');
+        var userEmail = $('[placeholder="Your email*"]');
+        var userSubject = $('select');
+        var userMessage = $('textarea');
+        var formScore = 0;
+
+        // Vérifier que l'utilisateur a bien saisi son nom
+        if( userName.val().length == 0 ){
+            // Ajouter la class error sur l'input
+            userName.addClass('error');
+
+        } else{
+            // Incrémenter la valeur de la variable formScore
+            formScore++;
+        };
+
+        // Vérifier que l'utilisateur a bien saisi au moins 4 caractères dans l'email
+        if( userEmail.val().length < 4 ){
+            // Ajouter la class error sur l'input
+            userEmail.addClass('error');
+
+        } else{
+            // Incrémenter la valeur de la variable formScore
+            formScore++;
+        };
+
+
+        // Vérifier que l'utilisateur a bien choisi un sujet
+        if( userSubject.val() == 'null' ){
+            // Ajouter la class error sur l'input
+            userSubject.addClass('error');
+
+        } else{
+            // Incrémenter la valeur de la variable formScore
+            formScore++;
+        };
+
+
+        // Vérifier que l'utilisateur a bien saisi au moins 5 caractères dans le message
+        if( userMessage.val().length < 5 ){
+            // Ajouter la class error sur l'input
+            userMessage.addClass('error');
+
+        } else{
+            // Incrémenter la valeur de la variable formScore
+            formScore++;
+        };
+
+
+        /*
+            Validation finale du formulaire
+        */
+        
+        if(formScore == 4){
+            
+
+            // Envoie des données dans le fichier de traitement PHP
+            // PHP répond true => continuer le traitement du formulaire
+
+            // => Afficher les données du formulaire dans une modal
+            $('div span').text( userName.val() );
+            $('div b').text( userSubject.val() );
+            $('div p:last').text( userMessage.val() );
+
+            // Afficher la modal
+            $('div').fadeIn();
+
+
+            // Vider les champs du formulaire
+            $('form')[0].reset();
+
+        };
+
+    });
+
+});
